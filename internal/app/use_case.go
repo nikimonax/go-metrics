ce1ffaab2c5a0b861257ca2ff98aad469550b4d6
nikimonax <n.monakhov@yandex.ru>
2026-07-04 21:23:12 +0300

    feat: add use cases to collect and send metrics

--- internal/app/use_case.go
+++ internal/app/use_case.go
@@ -4,6 +4,8 @@ import (
 	"github.com/nikimonax/go-metrics/internal/domain"
 )
 
+// update metrics
+
 type UpdateMetricUseCase struct {
 	metricRepository MetricRepository
 }
@@ -15,5 +17,59 @@ func (useCase *UpdateMetricUseCase) Execute(metric domain.Metric) error {
 func NewUpdateMetricUseCase(metricRepository MetricRepository) *UpdateMetricUseCase {
 	return &UpdateMetricUseCase{
 		metricRepository: metricRepository,
+	}
+}
+
+// collect metrics
+
+type CollectMetricsUseCase struct {
+	collector  MetricCollector
+	repository MetricRepository
+}
+
+func (useCase *CollectMetricsUseCase) Execute() error {
+	metrics, err := useCase.collector.Collect()
+
+	if err != nil {
+		return err
+	}
+
+	return useCase.repository.UpdateBatch(metrics)
+}
+
+func NewCollectMetricsUseCase(
+	collector MetricCollector,
+	repository MetricRepository,
+) *CollectMetricsUseCase {
+	return &CollectMetricsUseCase{
+		collector:  collector,
+		repository: repository,
+	}
+}
+
+// send metrics
+
+type SendMetricsUseCase struct {
+	gateway    MetricGateway
+	repository MetricRepository
+}
+
+func (useCase *SendMetricsUseCase) Execute() error {
+	metrics, err := useCase.repository.GetAll()
+
+	if err != nil {
+		return err
+	}
+
+	return useCase.gateway.SendBatch(metrics)
+}
+
+func NewSendMetricsUseCase(
+	gateway MetricGateway,
+	repository MetricRepository,
+) *SendMetricsUseCase {
+	return &SendMetricsUseCase{
+		gateway:    gateway,
+		repository: repository,
 	}
 }
