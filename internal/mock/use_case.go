d1b65b722b705310777e3ed840c5c1a4eb1ff093
nikimonax <n.monakhov@yandex.ru>
2026-07-08 11:28:30 +0300

    test: add use case mocks

--- internal/mock/use_case.go
+++ internal/mock/use_case.go
@@ -6,13 +6,59 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// update metric
+
 type UpdateMetricUseCase struct {
 	mock.Mock
 }
 
 // Execute implements [server.UpdateMetricUseCase].
-func (u *UpdateMetricUseCase) Execute(metric domain.Metric) error {
-	return u.Called(metric).Error(0)
+func (useCase *UpdateMetricUseCase) Execute(metric domain.Metric) error {
+	return useCase.Called(metric).Error(0)
 }
 
 var _ server.UpdateMetricUseCase = (*UpdateMetricUseCase)(nil)
+
+// get metric
+
+type GetMetricUseCase struct {
+	mock.Mock
+}
+
+// Execute implements [server.GetMetricUseCase].
+func (useCase *GetMetricUseCase) Execute(
+	metricType domain.MetricType,
+	metricName domain.MetricName,
+) (domain.Metric, error) {
+	args := useCase.Called(metricType, metricName)
+	var metric domain.Metric
+
+	if raw := args.Get(0); raw != nil {
+		metric = raw.(domain.Metric)
+	}
+
+	return metric, args.Error(1)
+}
+
+var _ server.GetMetricUseCase = (*GetMetricUseCase)(nil)
+
+// get all metrics
+
+type GetAllMetricsUseCase struct {
+	mock.Mock
+}
+
+// Execute implements [server.GetAllMetricsUseCase].
+func (useCase *GetAllMetricsUseCase) Execute() ([]domain.Metric, error) {
+	args := useCase.Called()
+
+	metrics := make([]domain.Metric, 0)
+
+	if raw := args.Get(0); raw != nil {
+		metrics = raw.([]domain.Metric)
+	}
+
+	return metrics, args.Error(1)
+}
+
+var _ server.GetAllMetricsUseCase = (*GetAllMetricsUseCase)(nil)
